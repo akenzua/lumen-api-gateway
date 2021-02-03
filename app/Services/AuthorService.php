57f6be0b0add310f195dd35f6d490d21ed6ede7b
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-02-03 11:50:29 +0000

    implement the update

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -27,5 +27,9 @@ class AuthorService {
         return $this->performRequest('GET', "/authors/{$author}");
 
     }
+
+    public function editAuthor($data, $author){
+        return $this->performRequest('PUT', "/authors/{$author}", $data);
+    }
     
 }
